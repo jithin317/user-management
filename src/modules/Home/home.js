8050f01066673ad99ebe3dc5fea4f8855e659a12
KONIJETI-NEELESH <k.neelesh009@gmail.com>
2024-04-12 03:59:21 +0530

    Home Page Added

--- src/modules/Home/home.js
+++ src/modules/Home/home.js
@@ -1,5 +1,16 @@
 import React from "react";
+import "../../index.css"
 
-export default function home() {
-  return <div>home</div>;
+export default function Home() {
+  return (
+    <div className="w-[100vw] h-[100vh] overflow-hidden m-0 p-0 border-0" id="home-bg">
+      <div className="w-[100%] h-[100%] bg-[#0000004f] flex flex-col justify-center items-center">
+        <h1 className="text-white xl:text-8xl lg:text-7xl md:text-6xl sm:text-5xl xs:text-4xl text-3xl font-bold" id="div">USER MANAGEMENT SYSTEM</h1>
+        <p className="text-white md:mx-10 mx-5 md:px-10 lg:text-3xl md:text-2xl text-xl text-center">A comprehensive platform leveraging the MERN stack for secure, efficient, and scalable management of user accounts, access permissions, and administrative tasks.</p>
+        <div className="m-5">
+          <button class='glowing-btn'><span class='glowing-txt'>GET<span class='faulty-letter'>START</span>ED</span></button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { lazy } from "react";
-import Home from "../modules/Home";
+// import { lazy } from "react";
+import Home from "../modules/Home/Home";
 // import ErrorRoute from "../modules/404";
 // const Login = lazy(() => import("../modules/auth-section/login"));
 // const SignUp = lazy(() => import("../modules/auth-section/signup"));
+import "../index.css"
 
 export default function Router() {
   const routes = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Home/>,
     },
     // {
     //   path: "/login",
